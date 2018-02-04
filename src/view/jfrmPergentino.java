deccf5607855e544424d3c95e579cb8b59bdf24e
Pergentino Liberato Edjang Nchama <pergenDeveloper@gmail.com>
2018-02-04 23:53:10 +0100

    Entrega del Sprint 1: Funcionalidad Implementada y funcional: 	+ Diseño de la ventana de resultados, incluyendo una tabla que contendrá el resultado de todos los calculos. 	+ Implementación de la función WMC (métodos ponderados por clase) y la inserción de los resultados en la tabla. En esta versión se muestra los resultados de WMC en la tabla y las demás métricas están a cero.
    NOTA:
    En el fichero jfrmPrincipal, he encontrado un bug, en el método jmnuPAbrirActionPerformed, en él se declara otra variable fichero y se le asigna el fichero seleccionado,
    y quedando el atributo declarado en la clase como fichero con el valor null.
    
    SOLUCIÓN: He eliminado la variable fichero, y lo he reemplazado con el atributo fichero quedando así con el valor del fichero seleccionado.
    
    Pergentino Liberato Edjang Nchama

--- src/view/jfrmPergentino.java
+++ src/view/jfrmPergentino.java
@@ -5,7 +5,11 @@
  */
 package view;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -16,25 +20,99 @@ public class jfrmPergentino extends javax.swing.JFrame {
     /**
      * Creates new form jfrmPergentino
      */
-    File f_a_tratar=null;
-    
+    private File f_a_tratar = null;
+    private final ArrayList<Clase> lista = new ArrayList<>();
+    private final DefaultTableModel model = new DefaultTableModel();
+
     public jfrmPergentino(File f) {
         initComponents();
-        f_a_tratar=f;
+        f_a_tratar = f;
+        tblResultados.setModel(model);
+
+        model.addColumn("Clase");
+        model.addColumn("WMC");
+        model.addColumn("DIT");
+        model.addColumn("NOC");
     }
-    
-    //métodos a implementar
-    private void WMC(){
-        //calculamos los métodos ponderados por clase 
+
+    //******  MÉTODOS A IMPLEMENTAR ******
+    private void WMC() {
+        //calculamos los métodos ponderados por clase
+        //Teniendo en cuenta la complejidad ciclomática, normalizo la complejidad a c=1
+        FileReader fr = null;
+        BufferedReader br;
+        int CC = 1; //Complejidad ciclomática
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            fr = new FileReader(f_a_tratar);
+            br = new BufferedReader(fr);
+            int posClase = 0;
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                //Ahora procedemos a tratar el fichero linea por linea dependiendo de nuestra necesidad
+                if (linea.contains("class") && linea.contains("{")) {
+                    posClase++;
+                    String[] tokens = linea.split(" ");
+                    for (int i = 0; i < tokens.length; i++) {
+                        if (tokens[i].equals("class")) {
+                            lista.add(new Clase(tokens[i + 1]));
+                        }
+                    }
+                } else if ((linea.contains("public") || linea.contains("private") || linea.contains("protected"))
+                        && (linea.contains("(") && linea.contains(")"))) {
+                    //Incrementamos el número de métodos mas uno y lo multiplicamos por su Complejidad Ciclomática
+                    lista.get(posClase - 1).setNumMetodos((lista.get(posClase - 1).getNumMetodos() + 1) * CC);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                System.out.println("Error: " + e2.getMessage());
+            }
+        }
+
     }
-    
-    private void DIT(){
-        //calculamos la profundidad del arbol de herencia de una clase
+
+    private void DIT() {
+        //Calculamos la profundidad del arbol de herencia de una clase
+
     }
-    
-    private void NOC(){
+
+    private void NOC() {
         //calculamos el número de hijos de una clase
     }
+
+    public void cargarTabla() {
+        
+        //Llamamos primero a los métodos que realizan las métricas
+        WMC(); DIT(); NOC();
+        
+        Object[] fila = new Object[4];
+        for (Clase item : lista) {
+            fila[0] = item.getNombre();
+            fila[1] = item.getNumMetodos();
+            fila[2] = item.getProfundidad();
+            fila[3] = item.getHijos();
+            //insertamos la fila en la tabla resultados
+            model.addRow(fila);
+        }
+
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -45,21 +123,52 @@ public class jfrmPergentino extends javax.swing.JFrame {
     private void initComponents() {
 
         jpPrincipal = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblResultados = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("MOOSE 1 (WMC, DIT Y NOC)");
+        setMaximumSize(new java.awt.Dimension(526, 208));
+        setMinimumSize(new java.awt.Dimension(526, 208));
 
-        jpPrincipal.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "MOOSE 1 (WMC, DIT Y NOC) Pergentino L. Edjang Nchama", 0, 0, new java.awt.Font("Tahoma", 1, 11))); // NOI18N
+        jpPrincipal.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "MOOSE 1 (WMC, DIT Y NOC) Pergentino L. Edjang Nchama", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11))); // NOI18N
+
+        tblResultados.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Clase", "WMC", "DIT", "NOC"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblResultados);
 
         javax.swing.GroupLayout jpPrincipalLayout = new javax.swing.GroupLayout(jpPrincipal);
         jpPrincipal.setLayout(jpPrincipalLayout);
         jpPrincipalLayout.setHorizontalGroup(
             jpPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 606, Short.MAX_VALUE)
+            .addGroup(jpPrincipalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 492, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jpPrincipalLayout.setVerticalGroup(
             jpPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 258, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpPrincipalLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(129, 129, 129))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -75,16 +184,62 @@ public class jfrmPergentino extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jpPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jpPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 196, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel jpPrincipal;
+    private javax.swing.JTable tblResultados;
     // End of variables declaration//GEN-END:variables
+}
+
+class Clase {
+
+    private String nombre = "";
+    private int profundidad = 0;
+    private int hijos = 0;
+    private int numMetodos = 0;
+
+    public Clase(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getProfundidad() {
+        return profundidad;
+    }
+
+    public void setProfundidad(int profundidad) {
+        this.profundidad = profundidad;
+    }
+
+    public int getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(int hijos) {
+        this.hijos = hijos;
+    }
+
+    public int getNumMetodos() {
+        return numMetodos;
+    }
+
+    public void setNumMetodos(int numMetodos) {
+        this.numMetodos = numMetodos;
+    }
+
 }
